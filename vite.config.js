3b3cdcfdcecc6eb7ced9e062bbba87791e593753
Fernando C <thelonder461@gmail.com>
2024-10-07 16:23:34 -0300

    fixing api proxy bug

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     minify: false, // Desativa a minificação durante o desenvolvimento
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://20.206.249.58:8666',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
   }
-  
 });
